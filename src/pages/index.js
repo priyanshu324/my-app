d7206d9769316831fa95a63031404ec359d2a5d4
priyanshu324 <sainipriyanshu324@gmail.com>
2025-05-20 13:22:44 +0530

    fourth commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold mb-4">Welcome to Next.js PWA</h1>
-      <Link href="/Login" className="text-blue-600 underline">
+      <h1 className="text-3xl font-bold mb-4 text-black">
+        Welcome to Next.js PWA
+      </h1>
+      <Link href="/Login" className="text-black underline ">
         Go to Login
       </Link>
     </div>
