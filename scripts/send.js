da05d9a8669a9beef766554901d424b67ee19841
priyanshu324 <sainipriyanshu324@gmail.com>
2025-05-20 13:55:04 +0530

    raws commit

--- scripts/send.js
+++ scripts/send.js
@@ -1,5 +1,5 @@
 import webpush from "web-push";
-import { subscriptions } from "../pages/api/save-subscription.js";
+import { subscriptions } from "../src/pages/api/save-subscription";
 
 const VAPID_PUBLIC_KEY =
   "BBxXmZVvJcvQbAFdX9rAiK62pui31gTfrL3VTDwzhmWJdOVGsBiwp9007JPjCSLglGX3tn7pSRREEDR4xsa6djc";
