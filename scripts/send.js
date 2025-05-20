f82ab62b89bc7ac3dc581ec59ddcc89068e3e383
priyanshu324 <sainipriyanshu324@gmail.com>
2025-05-20 14:17:38 +0530

    push-noti-1 commit

--- scripts/send.js
+++ scripts/send.js
@@ -3,11 +3,12 @@ const fs = require("fs");
 
 const subscriptions = JSON.parse(fs.readFileSync("data/subscriptions.json"));
 
-const VAPID_PUBLIC_KEY = "YOUR_PUBLIC_KEY";
-const VAPID_PRIVATE_KEY = "YOUR_PRIVATE_KEY";
+const VAPID_PUBLIC_KEY =
+  "BBxXmZVvJcvQbAFdX9rAiK62pui31gTfrL3VTDwzhmWJdOVGsBiwp9007JPjCSLglGX3tn7pSRREEDR4xsa6djc";
+const VAPID_PRIVATE_KEY = "dp0VkIs8SrUEsQ_1vFi95bp-nCnCRWzRX5i2_0sYSxM";
 
 webpush.setVapidDetails(
-  "mailto:admin@example.com",
+  "mailto:saini.sarkar777@gmail.com",
   VAPID_PUBLIC_KEY,
   VAPID_PRIVATE_KEY
 );
